640aa1c61acfa10b571e58a589056ea37532a01b
msncoder <msncoder@gmail.com>
2025-06-24 16:31:15 +0500

    add files and solve basic level problems in python

---
+++ ProblemSolution.py
@@ -0,0 +1,100 @@
+# Check Even or Odd
+
+# a = input("Enter a number: ")
+# a = int(a)
+# if a % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+# Multiplication Table
+
+# table = int(input("Enter a number"))
+
+# for i in range(1,11):
+#     print(f"{table} X {i} = {table*i}")
+
+
+    # natural_number = int(input("Enter a natural number"))
+    # sum_of_natural_number = 0
+
+    # for i in range(1,natural_number+1):
+    #     # print(i)
+    #     sum_of_natural_number += natural_number
+
+    # print(sum_of_natural_number)
+
+
+# Swap Two Numbers
+
+# a = 10
+# b = 20
+# temp = b
+# b = a
+# a = temp
+
+# print(a)
+# print(b)
+
+
+# Closest Number
+
+# n = 16
+# m = 4
+
+# q = int(n / m)
+# # print(q)
+# n1 = m * q
+# # print(n1)
+# if ((n*m) > 0):
+#     n2 = (m*(q+1))
+    
+# else:
+#     n2 = (m*(q-1))
+
+# if(abs(n-n1) < abs(n-n2)):
+#     print(n1)
+# else:
+#     print(n2)
+
+
+# Dice Problem
+
+# dice = int(input("Roll a dice: "))
+
+# if(dice == 1):
+#     print("6")
+
+# elif(dice == 2):
+#     print("5")
+
+# elif(dice == 3):
+#     print("4")
+
+# elif(dice == 4):
+#     print("3")
+
+# elif(dice == 5):
+#     print("2")
+
+# else:
+#     print("1")
+
+# Nth term of AP from First Two Terms
+
+# Input : a1 = 1, a2 = 3, n = 10
+# Output : 19
+
+# a1 = 1
+# a2 = 3 
+# n = 10
+# a = n * 2
+# output = 15
+
+# for i in range(1,a,2):
+#     if(i == output):
+#         print(i)
+        
+
